c3694b03c65d56fc17f95cbc92e7f3f372bcea1a
David Kimolo <mwikyakimolo@gmail.com>
2020-01-08 17:18:37 +0300

    update

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'library management system',
+    'version': '',
+    'summary': '',
+    'description': '',
+    'category': '',
+    'author': '',
+    'website': '',
+    'license': '',
+    'depends': ['base'],
+    'data': [''],
+    'demo': [''],
+    'installable': True,
+    'auto_install': False,
+    'external_dependencies': {
+        'python': [''],
+    }
+}
